763a22061671ecbd07870c5da07ea4b58f20bbf5
741871-debug <741871@student.dusd.net>
2026-05-09 12:41:03 -0700

    cz
    afd

---
+++ Unit5FloresP6/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
+    private float spawnRate = 1.0f;
+    private int score;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+    }
+}
